3d6d156c960a4af250a5d5053e3b80f9ed0cdeb6
N1kel24e <danyakiktenko529@gmail.com>
2026-03-22 22:28:25 +0100

    initial release

---
+++ app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import joblib
+import pandas as pd
+
+df = pd.read_csv('tracks_with_clusters.csv')
+
+model = joblib.load('kmeans.pkl')
+scaler = joblib.load('scaler.pkl')
+
+def song_search(song, artists):
+    result = df[df['track_name'].str.contains(song, na=False) & df['artists'].str.contains(artists, na=False)]
+    result = result.drop_duplicates(subset=['track_name', 'artists'])
+    result = result [['track_name', 'artists', 'cluster_name', 'energy', 'tempo', 'valence', 'danceability', 'acousticness',]]
+    return result
+
+def get_similar (cluster):
+    similar = df[df['cluster_name'] == cluster]
+    similar = similar.sample(n=5)
+    similar = similar[['track_name', 'artists',]]
+    return similar
+
+def song_analyze (song, artists):
+    search = song_search(song, artists)
+    cluster = search['cluster_name'].iloc[0]
+    similar = get_similar(cluster)
+    return search, similar
+
+st.title('Music Inspector')
+st.write('Analyze any track and find music that sounds just like it')
+
+artist = st.text_input('Enter artist')
+song = st.text_input('Enter song')
+
+if st.button('Analyze'):
+    track_info, similar = song_analyze(song, artist)
+    st.write('Song name: ')
+    st.write(track_info['track_name'].iloc[0])
+    st.write('Artist: ')
+    st.write(track_info['artists'].iloc[0])
+    st.write("Track info:")
+    st.write("Genre cluster: " + track_info['cluster_name'].iloc[0])
+    st.write("Energy")
+    st.progress(track_info['energy'].iloc[0])
+    st.write("Valence")
+    st.progress(track_info['valence'].iloc[0])
+    st.write("Danceability")
+    st.progress(track_info['danceability'].iloc[0])
+    st.write("Acousticness")
+    st.progress(track_info['acousticness'].iloc[0])
+    st.write("Similar tracks:")
+    st.write(similar)
